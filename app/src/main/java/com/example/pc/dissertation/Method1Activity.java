14babff1f9b05c4173e3dafbb0678fd88c056a2b
Ievgen bogatov <ievgen.bogatov@accellion.com>
2016-11-06 00:54:24 +0200

    Finish first method UI

--- app/src/main/java/com/example/pc/dissertation/Method1Activity.java
+++ app/src/main/java/com/example/pc/dissertation/Method1Activity.java
@@ -1,16 +1,24 @@
 package com.example.pc.dissertation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by PC on 05.11.2016.
  */
-public class Method1Activity extends Activity {
+public class Method1Activity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.method1_activity);
+        setContentView(R.layout.method2_activity);
+        findViewById(R.id.button_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Method1Activity.this, SelectStrategyActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pc/dissertation/Method2Activity.java
+++ app/src/main/java/com/example/pc/dissertation/Method2Activity.java
@@ -1,24 +1,16 @@
 package com.example.pc.dissertation;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 /**
  * Created by PC on 05.11.2016.
  */
-public class Method2Activity extends Activity{
+public class Method2Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.method2_activity);
-        findViewById(R.id.button_next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Method2Activity.this, SelectStrategyActivity.class));
-            }
-        });
+        setContentView(R.layout.method1_activity);
     }
 }
